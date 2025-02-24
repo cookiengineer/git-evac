7d2adda8078fd9e9b9308601d5771c323a91a5f8
Cookie Engineer <cookiengineer@protonmail.ch>
2025-02-25 00:56:05 +0100

    :bug: Fix for Dialog

--- app/components/Dialog.go
+++ app/components/Dialog.go
@@ -5,6 +5,7 @@ import "gooey/dom"
 import "gooey/elements"
 import "slices"
 import "sort"
+import "strconv"
 import "strings"
 
 var Dialog *elements.Dialog = nil
@@ -30,10 +31,26 @@ func InitDialog() {
 		if target.TagName == "BUTTON" {
 
 			if action == "open-terminal" {
+
+				// TODO: Open Terminal
+				// client.OpenTerminal(repository)?
+
 			} else if action == "fix" {
+
+				// TODO: Fix Repository
+
 			} else if action == "commit" {
+
+				// TODO: Commit Repository
+
 			} else if action == "pull" {
+
+				// TODO: Pull from remote
+
 			} else if action == "push" {
+
+				// TODO: Push to remote
+
 			} else if action == "cancel" {
 				Dialog.Close()
 			} else if action == "close" {
@@ -65,16 +82,33 @@ func RenderDialog(selected map[string]string) {
 
 		sort.Strings(ids)
 
-		html := ""
-
-		for i := 0; i < len(ids); i++ {
-			html += RenderDialogTableRow(ids[i])
-		}
-
+		h3 := Dialog.Element.QuerySelector("h3")
 		tbody := Dialog.Element.QuerySelector("table tbody")
 
+		if h3 != nil {
+
+			title := ""
+
+			if len(actions) == 1 {
+				title = strings.ToUpper(actions[0][0:1]) + strings.ToLower(actions[0][1:]) + " " + strconv.Itoa(len(selected)) + " Repositories"
+			} else {
+				title = "Manage " + strconv.Itoa(len(selected)) + " Repositories"
+			}
+
+			h3.SetInnerHTML(title)
+
+		}
+
 		if tbody != nil {
+
+			html := ""
+
+			for i := 0; i < len(ids); i++ {
+				html += RenderDialogTableRow(ids[i])
+			}
+
 			tbody.SetInnerHTML(html)
+
 		}
 
 		RenderDialogFooter(actions)
